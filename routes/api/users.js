ae3c1ae47377e7cbae3de0c7346b4ea01a1e071e
unknown <savinraul1983@yahoo.com>
2018-07-29 15:56:50 +0100

    login

--- routes/api/users.js
+++ routes/api/users.js
@@ -44,5 +44,30 @@ router.post('/register', (req, res) => {
     });
 });
 
+//@route  Get api/users/login
+//@desc   Login user / returning JWT(json web token) token
+//@access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  // Find user by email
+  User.findOne({ email: email}) // or User.findOne({ email })
+    .then(user => {
+      // Check for user
+      if (!user) {
+        return res.status(404).json({email: 'User not found'});
+      }
+      // Check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({msg: 'Success'})
+          } else {
+            return res.status(400).json({password: 'password incorrect'});
+          }
+        })
+    });
+});
+
 
 module.exports = router;
