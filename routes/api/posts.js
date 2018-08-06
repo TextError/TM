5b1b01aff730ef128fcee5a70af68616822ba61c
unknown <savinraul1983@yahoo.com>
2018-08-06 15:18:56 +0100

    comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -124,4 +124,46 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
     });
 });
 
+//@route  Post api/posts/comment/:id
+//@desc   Add comment to post
+//@access Private
+router.post('/comment/:id', passport.authenticate('jwt', {session:false}), (req,res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+  //check validation
+  if (!isValid) {
+    // If any errors, send 400 with errors object
+    return res.status(400).json(errors);
+  }
+  Post.findById(req.params.id)
+    .then(post => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      };
+      //Add to comment array
+      post.comments.unshift(newComment);
+
+      //Save
+      post.save()
+        .then(comment => res.json(comment))
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+//@route  Delete api/posts/comment/:id/:_id
+//@desc   Remove comment to post
+//@access Private
+router.post('/comment/:id/comment_id', passport.authenticate('jwt', {session:false}), (req,res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      //Check if the comment exists
+      if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id).lenght === 0) {
+        return res.status(404).json({ commentnotexists: 'Comment not exists '})
+      }
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 module.exports = router;
