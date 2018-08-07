b67a882c5f674ccf262d88a3f10860219a898e03
unknown <savinraul1983@yahoo.com>
2018-08-07 17:28:09 +0100

    Added navbar/footer and content

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import NavBar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>test!</p>
+        <NavBar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
